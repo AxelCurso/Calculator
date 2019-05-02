427ed0660cc8db72889a3e810c8f7aae7eb448a0
AxelCurso <bessyaxel69@gmail.com>
2019-05-02 15:59:43 +0200

    numbers in firstNum, need operand + display + other(class) + comma

--- calculator.js
+++ calculator.js
@@ -23,3 +23,53 @@ function operate(a, op, b) {
         return (div(a, b));
     }
 }
+
+let firstNum = 0;
+let comma = 0;
+
+const container = document.querySelector(".calculator");
+const buttons = container.querySelectorAll("div");
+buttons.forEach(button => button.addEventListener("click", function(e) {
+    let display = "";
+    // NUMBERS
+    if (Array.from(e.target.classList).includes("zero")) {
+        firstNum = firstNum * 10 + 0;
+    } else if (Array.from(e.target.classList).includes("one")) {
+        firstNum = firstNum * 10 + 1;
+    } else if (Array.from(e.target.classList).includes("two")) {
+        firstNum = firstNum * 10 + 2;
+    } else if (Array.from(e.target.classList).includes("three")) {
+        firstNum = firstNum * 10 + 3;
+    } else if (Array.from(e.target.classList).includes("four")) {
+        firstNum = firstNum * 10 + 4;
+    } else if (Array.from(e.target.classList).includes("five")) {
+        firstNum = firstNum * 10 + 5;
+    } else if (Array.from(e.target.classList).includes("six")) {
+        firstNum = firstNum * 10 + 6;
+    } else if (Array.from(e.target.classList).includes("seven")) {
+        firstNum = firstNum * 10 + 7;
+    } else if (Array.from(e.target.classList).includes("eight")) {
+        firstNum = firstNum * 10 + 8;
+    } else if (Array.from(e.target.classList).includes("nine")) {
+        firstNum = firstNum * 10 + 9;
+    } // POINT
+    else if (Array.from(e.target.classList).includes("point")) {
+        comma += 1;
+    } // OPERAND
+    else if (Array.from(e.target.classList).includes("plus")) {
+        display = 
+    } else if (Array.from(e.target.classList).includes("plus")) {
+
+    } else if (Array.from(e.target.classList).includes("plus")) {
+
+    } else if (Array.from(e.target.classList).includes("plus")) {
+
+    }
+    if (comma == 2) {
+        display = "ERROR 2 COMMAS";
+        comma = 0;
+        firstNum = 0;
+    }
+    console.log(firstNum);
+    console.log(display);
+}));
